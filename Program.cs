8a7e98e5a0b29f3390da6d1e41148f7f6ab4112e
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2023-10-04 19:39:22 +0200

    Added FastEndpoints.

--- Program.cs
+++ Program.cs
@@ -1,58 +1,63 @@
 using CocktailApi;
 using FastEndpoints;
+using FastEndpoints.Swagger;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 
-var builder = WebApplication.CreateBuilder(args);
+internal class Program
 {
-  builder.Services.AddMvc();
-  builder.Services.AddAuthentication(options =>
-  {
-      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-  }).AddJwtBearer(options =>
-  {
-      options.Authority = "https://dev-rglgi3ifuoda0lfd.eu.auth0.com/";
-      options.Audience = "https://www.dirtydrinking.com";
-  });
-
-  builder.Services
-    .AddAuthorization(options =>
+    private static void Main(string[] args)
     {
-        options.AddPolicy("delete:cocktail",
-        policy => policy.Requirements.Add(new HasPermissionRequirement("delete:cocktail"))
-        );
-    })
-    .AddCors(options =>
-    {
-        options.AddPolicy(
-            name: "AllowLocalhost",
-            policy  =>
+        var builder = WebApplication.CreateBuilder(args);
+        {
+            builder.Services.AddMvc();
+            builder.Services.AddAuthentication(options =>
             {
-                policy.WithOrigins("http://localhost:5173");
-                policy.AllowAnyHeader();
-                policy.AllowCredentials();
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = "https://dev-rglgi3ifuoda0lfd.eu.auth0.com/";
+                options.Audience = "https://www.dirtydrinking.com";
             });
-    })
-    .AddSingleton<IAuthorizationHandler, HasPermissionHandler>()
-    .AddFastEndpoints()
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen();
-}
 
-var app = builder.Build();
-{
-    app
-        .UseAuthentication()
-        .UseAuthorization()
-        .UseCors("AllowLocalhost")
-        .UseFastEndpoints();
+            builder.Services
+              .AddAuthorization(options =>
+              {
+                  options.AddPolicy("delete:cocktail",
+                  policy => policy.Requirements.Add(new HasPermissionRequirement("delete:cocktail"))
+                  );
+              })
+              .AddCors(options =>
+              {
+                  options.AddPolicy(
+                      name: "AllowLocalhost",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173");
+                          policy.AllowAnyHeader();
+                          policy.AllowCredentials();
+                      });
+              })
+              .AddSingleton<IAuthorizationHandler, HasPermissionHandler>()
+              .AddFastEndpoints()
+              .SwaggerDocument();
+        }
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        var app = builder.Build();
+        {
+            app
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseCors("AllowLocalhost")
+                .UseFastEndpoints();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerGen();
+            }
+        }
+
+        app.Run();
     }
 }
-
-app.Run();
